5eeea809170de185764f9d007c26bdae2d7404f4
abualsamid <ahmad@abualsamid.com>
2012-07-14 17:40:36 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Perfecttrax::Application.config.secret_token = '3ba3bf6f92da98d79cf7eb340fe2545d39f353d131243f8e3eca441ded459f7e8c766c2cd47e46ed20f5d52fc83b8843ea68ef7f4466aefd5229cff283f81eaa'
