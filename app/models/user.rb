7f7411929a2f83a9362505966d48c1a920631ba0
abualsamid <ahmad@abualsamid.com>
2012-07-27 23:25:16 -0500

    feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,12 @@ class User < ActiveRecord::Base
 	attr_accessible :name, :email, :password, :password_confirmation
 	has_secure_password
     has_many :microposts, dependent: :destroy
+    has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+    has_many :followed_users, through: :relationships, source: :followed
+
+    has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationship", dependent: :destroy
+
+    has_many :followers, through: :reverse_relationships, source: :follower
 
 	before_save { |user| user.email = email.downcase }
 
@@ -14,6 +20,18 @@ class User < ActiveRecord::Base
 	validates :password_confirmation, presence: true  
 	
 	def feed 
-		microposts
+		Micropost.from_users_followed_by(self)
 	end
+
+    def following?(other_user)
+      relationships.find_by_followed_id(other_user.id)
+    end
+    def follow!(other_user) 
+      relationships.create!(followed_id: other_user.id)
+      # equivalent to self.relationships.create!(followed_id: other_user.id)
+    end
+
+    def unfollow!(other_user) 
+      self.relationships.find_by_followed_id(other_user.id).destroy
+    end
 end
