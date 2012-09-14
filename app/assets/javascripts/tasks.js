32b7adc5e3e338b57a47c3a55b69e96aa0dc2933
abualsamid <ahmad@abualsamid.com>
2012-09-14 09:59:23 -0500

    more DnD

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -22,55 +22,73 @@ function toggleStatus(id,v) {
 	
 }
 
-$(function() {
+function setupDrag(){
+	try {
+		$( "i.draggable" ).draggable({
+				appendTo: "body",
+				helper: "clone",
+				cursor: "move",
+		});
 
+	} catch(x){}
 	
-	$( "li.draggable" ).draggable({
-			appendTo: "body",
-			helper: "clone",
-			cursor: "move",
-	});
-
-	$( "ol.tasks" ).sortable({
-			sort: function() {
-				// gets added unintentionally by droppable interacting with sortable
-				// using connectWithSortable fixes this, but doesn't allow you to customize active/hoverClass options
-				$( this ).removeClass( "ui-state-default" );
-			}
-	});
-	$( "li.stack" ).droppable({
-				tolerance: "intersect",
-				accepts: "li.task",
-				activeClass: "ui-state-default",
-			    hoverClass: "ui-state-hover",
-				drop: function( event, ui ) {
-					var task_id=$(ui.draggable).attr('task_id');
-					var stack_id=$(ui.draggable).attr('stack_id');
-					var new_stack_id=$(this).attr('stack_id');
-					try {
-						$.ajax({
-					   		type: "PUT",
-					   		url: "/stacks/" + stack_id + "/tasks/" + task_id + ".js",
-					   		data: {
-					   			new_stack_id: new_stack_id
-					   			
-					   		},
-					   		success: function(msg){
-						 		// alert( "Data Saved: " + msg );
-								$(ui.draggable).remove();
-					
-					   		},
-					   		error: function (xhr, ajaxOptions, thrownError) {
-        						alert(xhr.status + ": " + thrownError);
-      						}
-						 });
-					} catch(x) {
-						alert(x);
-					}
+	try {
+		$( "ol.tasks" ).sortable({
+				sort: function() {
+					// gets added unintentionally by droppable interacting with sortable
+					// using connectWithSortable fixes this, but doesn't allow you to customize active/hoverClass options
+					$( this ).removeClass( "ui-state-default" );
 				}
-	});
+		});
+	
+	} catch(x){}
+
+}
+function setupDrop() {
+	try {
+		$( "li.stack" ).droppable({
+					tolerance: "intersect",
+					accepts: "i.draggable",
+					activeClass: "ui-state-default",
+					hoverClass: "ui-state-hover",
+					drop: function( event, ui ) {
+						var task_id=$(ui.draggable).attr('task_id');
+						var stack_id=$(ui.draggable).attr('stack_id');
+						var new_stack_id=$(this).attr('stack_id');
+						try {
+							$.ajax({
+						   		type: "PUT",
+						   		url: "/stacks/" + stack_id + "/tasks/" + task_id + ".js",
+						   		data: {
+						   			new_stack_id: new_stack_id
+						   			
+						   		},
+						   		success: function(msg){
+							 		// alert( "Data Saved: " + msg );
+									$(ui.draggable).parent().remove();
+					
+						   		},
+						   		error: function (xhr, ajaxOptions, thrownError) {
+		    						alert(xhr.status + ": " + thrownError);
+		  						}
+							 });
+						} catch(x) {
+							alert(x);
+						}
+					}
+		});
 	
 	
+	}catch(x) {}
+
 }
 
-)
+function setupDragNDrop() {
+	setupDrag();
+	setupDrop();		
+}
+
+$(function() {
+	setupDragNDrop();
+	
+});
